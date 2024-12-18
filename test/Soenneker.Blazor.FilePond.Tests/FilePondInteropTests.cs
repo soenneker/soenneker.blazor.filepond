86598c8b07da9f8e1f5613001a0f237ec67a9b5a
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:42 +0000

    xUnit v3

--- test/Soenneker.Blazor.FilePond.Tests/FilePondInteropTests.cs
+++ test/Soenneker.Blazor.FilePond.Tests/FilePondInteropTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.Blazor.FilePond.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.Blazor.FilePond.Tests;
 
--- test/Soenneker.Blazor.FilePond.Tests/Fixture.cs
+++ test/Soenneker.Blazor.FilePond.Tests/Fixture.cs
@@ -8,7 +8,7 @@ namespace Soenneker.Blazor.FilePond.Tests;
 
 public class Fixture : UnitFixture
 {
-    public override System.Threading.Tasks.Task InitializeAsync()
+    public override System.Threading.Tasks.ValueTask InitializeAsync()
     {
         SetupIoC(Services);
 
