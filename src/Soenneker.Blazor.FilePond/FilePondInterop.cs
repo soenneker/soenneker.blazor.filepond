84674a567e602bca19bae57e353d2fcce0d94610
soenneker <jake@soenneker.com>
2026-09-25 21:48:48 -0500

    Updated Soenneker.Quark.Suite to 4.0.1465

--- src/Soenneker.Blazor.FilePond/FilePondInterop.cs
+++ src/Soenneker.Blazor.FilePond/FilePondInterop.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -97,7 +98,7 @@ public sealed class FilePondInterop : IFilePondInterop
         await module.InvokeVoidAsync(identifier, cancellationToken, args);
     }
 
-    private async ValueTask<T> InvokeAsync<T>(string identifier, CancellationToken cancellationToken, params object?[] args)
+    private async ValueTask<T> InvokeAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties)] T>(string identifier, CancellationToken cancellationToken, params object?[] args)
     {
         IJSObjectReference module = await GetModule(cancellationToken);
         return await module.InvokeAsync<T>(identifier, cancellationToken, args);
